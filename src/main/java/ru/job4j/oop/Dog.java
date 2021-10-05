e70863384aa3d7dc4393c5219611cbdbc6b1e9c9
AkiBoba <lettervl@ya.ru>
2021-10-05 11:05:13 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog big = new Dog();
+        Dog litlie = new Dog();
+        Dog midlie = new Dog();
+    }
+}
