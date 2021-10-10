4f4522be01d52099c982e53149c8896a87093265
Vladimir Likhachev <lettervl@ya.ru>
2021-10-10 21:36:37 +0300

    # 5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -1,18 +1,23 @@
 package ru.job4j.inheritance;
 
 public class TextReport {
+
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
 }
 
 class HtmlReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>" + "<br/>" + "<span>" + body + "</span>";
     }
 }
 
 class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "name: " + name + System.lineSeparator() + "body: " + body;
     }
