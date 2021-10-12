d5df1b8965a618ad02f01c4f8fd08c317575dbf5
Vladimir Likhachev <lettervl@ya.ru>
2021-10-12 21:01:40 +0300

    4. Переопределение [#174890 #214535]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -19,11 +19,13 @@ class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "\"name\" : "
+        return "{" + System.lineSeparator()
+                + "\"name\" : "
                 + "\"" + name + "\"" + ","
                 + System.lineSeparator()
                 + "\"body\" : "
-                + "\"" + body + "\"";
+                + "\"" + body + "\"" + System.lineSeparator()
+                + "}";
     }
 }
 
