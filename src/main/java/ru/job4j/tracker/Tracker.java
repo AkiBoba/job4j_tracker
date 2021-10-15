a980b856555253adabfc0b826ffa3b46c5e5d0dd
AkiBoba <lettervl@ya.ru>
2021-10-15 12:19:46 +0300

    8. Что такое валидация? [#246864 #216339]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -52,7 +52,6 @@ public class Tracker {
         if (rsl) {
            item.setId(id);
            items[index] = item;
-            result = true;
         }
         return result;
     }
@@ -65,7 +64,6 @@ public class Tracker {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            result = true;
         }
         return result;
     }
