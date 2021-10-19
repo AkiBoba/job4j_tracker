9fc27dc78777dc2396289ca0d438dc5329a9d07b
AkiBoba <lettervl@ya.ru>
2021-10-19 11:29:32 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #217615]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -46,14 +46,14 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        boolean result = false;
+//        boolean result = false;
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
            item.setId(id);
            items[index] = item;
         }
-        return result;
+        return rsl;
     }
 
     public boolean delete(int id) {
