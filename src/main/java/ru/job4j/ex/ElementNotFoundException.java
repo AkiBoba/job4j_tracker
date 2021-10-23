2712454ab77b14d128d7e1cc42ed58ae35f84cc4
Vladimir Likhachev <lettervl@ya.ru>
2021-10-23 10:18:38 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+/**
+ * @author Vladimir Likhachev
+ */
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+/**
+ * @author Vladimir Likhachev
+ */
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl < 0) {
+            throw new ElementNotFoundException("Element Not Found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"t1", "t2", "t3"};
+        try {
+            indexOf(arr, "t0");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
