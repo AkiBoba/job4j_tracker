3061ddc25dedceb477f9a302110f81a9d362bf91
AkiBoba <lettervl@ya.ru>
2021-11-09 16:10:38 +0300

    0. Сортировка [#10096 #225737]

--- src/main/java/ru/job4j/tracker/SortByReversNameItem.java
+++ src/main/java/ru/job4j/tracker/SortByReversNameItem.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class SortByReversNameItem implements Comparator<Item> {
 
     @Override
-    public int compare(Item o2, Item o1) {
-        return o1.getName().compareTo(o2.getName());
+    public int compare(Item o1, Item o2) {
+        return o2.getName().compareTo(o1.getName());
     }
 }
