2c62f5189f68aeac4587d70e58a50b2f80fca3f1
Vladimir Likhachev <lettervl@ya.ru>
2021-11-28 12:51:24 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+/**
+ * @author Vladimir Likhachev
+ */
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> stringComparator = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+    }
+}
