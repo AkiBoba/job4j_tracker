2f476fed5f10137e3ba226d5815a0b0844fecd00
AkiBoba <lettervl@ya.ru>
2021-11-30 09:31:39 +0300

    0. Stream API [#58166]

--- src/main/java/ru/job4j/stream/Streamlst.java
+++ src/main/java/ru/job4j/stream/Streamlst.java
@@ -8,7 +8,7 @@ public class Streamlst {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, -4, 2, -5, 3, -3, 4, 5);
         list = list.stream()
-                .filter(el -> el >= 0)
+                .filter(el -> el > 0)
                 .collect(Collectors.toList());
     }
 }
